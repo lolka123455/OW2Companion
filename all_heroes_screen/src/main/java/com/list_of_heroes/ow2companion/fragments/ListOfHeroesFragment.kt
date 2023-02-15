ae4cea204560f6242d597c1d8a70192d5cd73a5d
Alexander Tyryatkin <altyryatkin@gmail.com>
2023-02-15 15:42:47 +0300

    Add heroes item and adapters

--- all_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/ListOfHeroesFragment.kt
+++ all_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/ListOfHeroesFragment.kt
@@ -5,17 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
+import com.list_of_heroes.ow2companion.R
+import com.list_of_heroes.ow2companion.adapter.ViewPagerAdapter
 import com.list_of_heroes.ow2companion.databinding.FragmentListOfHeroesBinding
 
 class ListOfHeroesFragment : Fragment() {
 
-    private lateinit var binding: FragmentListOfHeroesBinding
+    private val binding: FragmentListOfHeroesBinding
+        get() = _binding!!
+    private var _binding: FragmentListOfHeroesBinding? = null
+
+    private val fragmentsList = listOf(
+        TanksFragment(),
+        DmgFragment(),
+        SupportFragment()
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentListOfHeroesBinding.inflate(inflater, container, false)
+        _binding = FragmentListOfHeroesBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initialViewPager()
+    }
+
+    private fun initialViewPager() {
+        val titles = resources.getStringArray(R.array.tab_titles)
+        val adapter =ViewPagerAdapter(requireActivity(),fragmentsList)
+        binding.pager.adapter = adapter
+        TabLayoutMediator(binding.tabLayout, binding.pager) {
+                tabLayout, position -> tabLayout.text = titles[position]
+        }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
