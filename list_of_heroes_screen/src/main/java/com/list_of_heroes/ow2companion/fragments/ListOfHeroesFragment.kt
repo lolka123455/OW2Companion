78e7bf0b688f965a6c403945a817ac16c7906abf
Alexander Tyryatkin <altyryatkin@gmail.com>
2023-02-15 14:34:00 +0300

    Added foundation

--- list_of_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/ListOfHeroesFragment.kt
+++ list_of_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/ListOfHeroesFragment.kt
@@ -1,4 +1,4 @@
-package com.list_of_heroes.ow2companion
+package com.list_of_heroes.ow2companion.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
---
+++ list_of_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/network/models/AllHeroesItem.kt
@@ -0,0 +1,8 @@
+package com.list_of_heroes.ow2companion.network.models
+
+data class AllHeroesItem(
+    val key: String,
+    val name: String,
+    val portrait: String,
+    val role: String
+)
