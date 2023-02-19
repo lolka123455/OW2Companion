b5566a609d481d4a2817b067a4700a7ebf9f3ef2
Alexander Tyryatkin <altyryatkin@gmail.com>
2023-02-19 18:06:18 +0300

    Navigation is urgently placed in a separate module

--- app/src/main/java/com/ow2companion/MainActivity.kt
+++ app/src/main/java/com/ow2companion/MainActivity.kt
@@ -2,8 +2,6 @@ package com.ow2companion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.ow2companion.databinding.ActivityMainBinding
