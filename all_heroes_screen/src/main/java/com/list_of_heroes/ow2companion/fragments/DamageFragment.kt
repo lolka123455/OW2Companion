4dfdd75233d8632465b1242bebfd341920e53225
alexander <altyryatkin@gmail.com>
2023-03-06 11:24:27 +0300

    Fixed DMG screen heroes

--- all_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/DamageFragment.kt
+++ all_heroes_screen/src/main/java/com/list_of_heroes/ow2companion/fragments/DamageFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.list_of_heroes.ow2companion.adapter.AllHeroesAdapter
 import com.list_of_heroes.ow2companion.databinding.FragmentDamageBinding
 import com.list_of_heroes.ow2companion.network.models.AllHeroesItem
 import com.list_of_heroes.ow2companion.viewmodels.DamageHeroesViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DamageFragment : Fragment() {
+class DamageFragment : Fragment(), AllHeroesAdapter.HeroItemListener {
 
     private val binding: FragmentDamageBinding
         get() = _binding!!
@@ -35,18 +38,32 @@ class DamageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.gameModesRecyclerView.adapter = adapter
         viewModel.getAllHeroes()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = AllHeroesAdapter(this)
+        binding.gameModesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.gameModesRecyclerView.adapter = adapter
+
         observeAllHeroes()
     }
 
     private fun observeAllHeroes(){
         lifecycleScope.launchWhenCreated {
-            viewModel.allHeroesList.collect{
+            viewModel.allHeroesList.collect {
                 listAllHeroes = it
                 adapter.allHeroesList = listAllHeroes
             }
         }
+    }
+
+    override fun onClickedHero(heroName: String) {
+        val bundle = Bundle()
+        bundle.putString("key",heroName)
+        findNavController().navigate(com.navigation.ow2companion.R.id.action_listOfHeroesFragment_to_detailsHeroFragment,bundle)
+        Toast.makeText(context,"CLICKED",Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroyView() {
