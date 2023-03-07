d71b0b4adf011d2582c33eb12b04b179cbff69df
alexander <altyryatkin@gmail.com>
2023-03-07 15:37:04 +0300

    Add binding and navigate backPressed

--- search_player_screen/src/main/java/com/search_player/ow2companion/fragments/SearchPlayerDetailsFragment.kt
+++ search_player_screen/src/main/java/com/search_player/ow2companion/fragments/SearchPlayerDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.search_player.ow2companion.databinding.FragmentSearchPlayerDetailsBinding
 
 class SearchPlayerDetailsFragment : Fragment() {
@@ -20,6 +21,17 @@ class SearchPlayerDetailsFragment : Fragment() {
     ): View {
         _binding = FragmentSearchPlayerDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onBackPressedToSearchPlayer()
+    }
+
+    private fun onBackPressedToSearchPlayer(){
+        binding.ibBack.setOnClickListener{
+            findNavController().navigateUp()
+        }
     }
 
 }
