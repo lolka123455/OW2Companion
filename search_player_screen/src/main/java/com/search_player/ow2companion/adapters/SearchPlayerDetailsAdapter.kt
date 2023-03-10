3e81824d6c699d7fefaec6de20034c5e5c0b3734
alexander <altyryatkin@gmail.com>
2023-03-10 21:08:46 +0300

    Added the ability to display ranks for each role. Made refactoring in the adapter

--- search_player_screen/src/main/java/com/search_player/ow2companion/adapters/SearchPlayerDetailsAdapter.kt
+++ search_player_screen/src/main/java/com/search_player/ow2companion/adapters/SearchPlayerDetailsAdapter.kt
@@ -3,6 +3,7 @@ package com.search_player.ow2companion.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -49,28 +50,27 @@ class SearchPlayerDetailsAdapter(private val listener: PlayerItemListener) :
             binding.root.setOnClickListener(this)
         }
 
+        private fun loadGlideImage(url: Any?, imageView: ImageView) {
+            Glide.with(binding.root)
+                .load(url)
+                .into(imageView)
+        }
+
         fun bind(item: SearchPlayer) {
             this.playerItem = item
             binding.playerUsername.text = item.username
             binding.playerTitle.text = item.title
 
-            Glide.with(binding.root)
-                .load(item.avatar)
-                .into(binding.playerAvatar)
+            loadGlideImage(item.avatar, binding.playerAvatar)
+            loadGlideImage(item.competitive.pc.tank.rank_icon, binding.ivTankRank)
+            loadGlideImage(item.competitive.pc.damage.rank_icon, binding.ivDamageRank)
+            loadGlideImage(item.competitive.pc.support.rank_icon, binding.ivSupportRank)
 
             when (item.endorsement.level) {
-                1 -> Glide.with(binding.root)
-                    .load(R.drawable.ic_1_lvl_rep)
-                    .into(binding.ivLvlReputation)
-                2 -> Glide.with(binding.root)
-                    .load(R.drawable.ic_2_lvl_rep)
-                    .into(binding.ivLvlReputation)
-                3 -> Glide.with(binding.root)
-                    .load(R.drawable.ic_3_lvl_rep)
-                    .into(binding.ivLvlReputation)
-                4 -> Glide.with(binding.root)
-                    .load(R.drawable.ic_4_lvl_rep)
-                    .into(binding.ivLvlReputation)
+                1 -> loadGlideImage(R.drawable.ic_1_lvl_rep, binding.ivLvlReputation)
+                2 -> loadGlideImage(R.drawable.ic_2_lvl_rep, binding.ivLvlReputation)
+                3 -> loadGlideImage(R.drawable.ic_3_lvl_rep, binding.ivLvlReputation)
+                4 -> loadGlideImage(R.drawable.ic_4_lvl_rep, binding.ivLvlReputation)
                 else -> binding.ivLvlReputation.visibility = View.GONE
             }
         }
